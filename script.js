8e1dc606a73f41b25c88a110b0c8e59cde67ab8b
Julia Shumeyko <46918344+JuliaShumeyko@users.noreply.github.com>
2019-09-25 00:13:07 +0300

    added examples of console methods

--- script.js
+++ script.js
@@ -8,24 +8,65 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
     // Regular
 
+    console.log('hello');
+
     // Interpolated
+
+    console.log('Hello, I am an %s string', 'inserted');
 
     // Styled
 
+    console.log('%cHello, I am just some funky yellow text', 'color:#bada55; text-shadow: 1px 1px 0 grey');
+
     // warning!
+
+    console.warn('Achtung!');
 
     // Error :|
 
+    console.error('Oops! Error!');
+
+
     // Info
+
+    console.info('Random fact: There was a successful Tinder match in Antarctica in 2014');
 
     // Testing
 
+    const p = document.querySelector('p');
+    console.assert(p.classList.contains('ouch'), 'That is wrong!');
+
     // clearing
+
+    // console.clear();
 
     // Viewing DOM Elements
 
+    console.log(p);
+    console.dir(p);
+
     // Grouping together
+    dogs.forEach(dog => {
+    console.groupCollapsed(`${dog.name}`);
+    console.log(`This is ${dog.name}`);
+    console.log(`${dog.name} is ${dog.age} years old.`);
+    console.log(`${dog.name} is ${7 * dog.age} years old.`);
+    console.groupEnd(`${dog.name}`);
+    })
 
     // counting
 
+    console.count('Julia');
+    console.count('Julia');
+    console.count('Julia');
+    console.count('Julia');
+
     // timing
+
+    console.time('fetching data');
+    fetch('https://api.github.com/users/JuliaShumeyko')
+        .then(data => data.json())
+        .then(data => {
+            console.timeEnd('fetching data');
+            console.log(data);
+        })
